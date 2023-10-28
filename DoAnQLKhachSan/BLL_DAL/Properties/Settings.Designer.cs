e64826e27dd57080b4eb143ed3d7fdd1dc563a99
DangNgocThao <dangthao352002@gmail.com>
2023-10-28 16:56:36 +0700

    thử dùng github

--- DoAnQLKhachSan/BLL_DAL/Properties/Settings.Designer.cs
+++ DoAnQLKhachSan/BLL_DAL/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace BLL_DAL.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-AILSKO80;Initial Catalog=QLKhachSan;User ID=sa;Password=123")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-GIGABYTE;Initial Catalog=QLKhachSan;User ID=sa;Password=123")]
         public string QLKhachSanConnectionString {
             get {
                 return ((string)(this["QLKhachSanConnectionString"]));
